6b43061c147914b168c9653c031bb0126966ace4
Valerii Vasin <valerii.vasin@gmail.com>
2015-03-10 01:19:49 +0100

    Implement bubbleEvents.

--- src/ChildMixin.js
+++ src/ChildMixin.js
@@ -2,56 +2,101 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 
 export default {
-    /**
-     * Instantiate subview within current view
-     * @param  {Backbone.View} View Subview
-     * @param  {Object} params      View params
-     */
-    initSubview: function(View, params) {
-        if (typeof params === 'undefined') {
-          params = {};
-        }
-
-        // find selector inside parent view
-        if (typeof params.el === 'string') {
-            params.el = this.$(params.el);
-        }
-
-        let subview = new View(params);
-
-        // remove reference from parent
-        // Notice: .remove() has been already handled
-        this.listenTo(subview, '__remove__', function() {
-            this.__subviews = _.without(this.__subviews, subview);
-        }, this);
-
-        if (!this.__subviews) {
-            this.__subviews = [];
-        }
-        this.__subviews.push(subview);
-
-        return subview;
-    },
-
-    destroySubviews: function(subview) {
-        if (!this.__subviews) {
-            return;
-        }
-
-        if (subview) {
-            this.__subviews = _.without(this.__subviews, subview);
-            subview.remove();
-            return;
-        }
-
-        _.each(this.__subviews, function(subview) {
-            subview.remove();
-        });
-    },
-
-    remove: function() {
-        this.destroySubviews();
-        this.trigger('__remove__', this);
-        Backbone.View.prototype.remove.apply(this, arguments);
+  /**
+   * Instantiate subview within current view
+   * @param  {Backbone.View} View Subview
+   * @param  {Object} params    View params
+   */
+  initSubview(View, params) {
+    if (typeof params === 'undefined') {
+      params = {};
     }
+
+    // find selector inside parent view
+    if (typeof params.el === 'string') {
+      params.el = this.$(params.el);
+    }
+
+    let subview = new View(params);
+
+    this._createChannel(subview);
+
+    // remove reference from parent
+    // Notice: .remove() has been already handled
+    this.listenTo(subview, '__remove__', () => {
+      this.__subviews = _.without(this.__subviews, subview);
+    });
+
+    if (!this.__subviews) {
+      this.__subviews = [];
+    }
+    this.__subviews.push(subview);
+
+    return subview;
+  },
+
+  destroySubviews(subview) {
+    if (!this.__subviews) {
+      return;
+    }
+
+    if (subview) {
+      this.__subviews = _.without(this.__subviews, subview);
+      subview.remove();
+      return;
+    }
+
+    _.each(this.__subviews, (subview) => {
+      subview.remove();
+    });
+  },
+
+  _setupEventProxy(subview) {
+    let current = _.keys(this.bubbleEvents || {});
+    let parent = this.__parentBubbleEvents || [];
+
+    // listen to subview events and proxy them to parent view
+    _.each(parent, (eventName) => {
+      this.listenTo(subview, eventName, function() {
+
+        // prepend eventName and trigger
+        let args = _.toArray(arguments);
+        args.unshift(eventName);
+        this.trigger.apply(this, args);
+      });
+    });
+
+    // set internal property
+    subview.__parentBubbleEvents = _.union(current, parent);
+
+    // Handle subviews of subview proxies
+    // (if they was created during initialization)
+    // because during initialization __parentBubbleEvents was not set
+    _.each(subview.__subviews, (subviewOfSubview) => {
+      subview._setupEventProxy(subviewOfSubview);
+    });
+  },
+
+  /**
+   * Create internal parent <> subview channel
+   *
+   * Notice:
+   * we do not need proper destroyChannel method
+   * because we use .listenTo() method for subscriptions
+   * and after view .remove() it will unsubscribe everything automatically
+   */
+  _createChannel(subview) {
+    // setup proxy events
+    this._setupEventProxy(subview);
+
+    _.each(this.bubbleEvents, (methodName, eventName) => {
+      this.listenTo(subview, eventName, this[methodName]);
+    });
+  },
+
+  remove() {
+    this.destroySubviews();
+    this.trigger('__remove__');
+    Backbone.View.prototype.remove.apply(this, arguments);
+  }
 };
--- test/ChildMixin_test.js
+++ test/ChildMixin_test.js
@@ -2,103 +2,168 @@ import Backbone from 'backbone';
 import ChildMixin from '../src/ChildMixin';
 
 describe('ChildMixin', function () {
-  let View;
-  let view;
-  let SubView;
-  let subview;
 
-  /**
-   * Helper function
-   * @param  {Backbone.View} View Backbone view that has subviews
-   * @return {Number}        Subviews count
-   */
-  function subviewCount(View) {
-    if (!View.__subviews) {
-      return 0;
+  describe('Subview init/remove', () => {
+    let View;
+    let view;
+    let SubView;
+    let subview;
+
+    /**
+     * Helper function
+     * @param  {Backbone.View} View Backbone view that has subviews
+     * @return {Number}        Subviews count
+     */
+    function subviewCount(View) {
+      if (!View.__subviews) {
+        return 0;
+      }
+
+      return View.__subviews.length;
     }
 
-    return View.__subviews.length;
-  }
+    beforeEach(() => {
+      View = Backbone.View.extend(ChildMixin);
+      SubView = Backbone.View.extend(ChildMixin);
 
-  beforeEach(() => {
-    View = Backbone.View.extend(ChildMixin);
-    SubView = Backbone.View.extend(ChildMixin);
+      view = new View();
 
-    view = new View();
+      spyOn(SubView.prototype, 'initialize');
+      subview = view.initSubview(SubView);
+    });
 
-    spyOn(SubView.prototype, 'initialize');
-    subview = view.initSubview(SubView);
+    describe('#initSubview', () => {
+      it('should instantiate subview correctly', () => {
+        expect(subview.initialize).toHaveBeenCalled();
+        expect(subviewCount(view)).toBe(1);
+      });
+
+      it('should return instance of subview', () => {
+        expect(subview instanceof SubView).toBe(true);
+      });
+
+      it('should instantiate inside parent view if .el is a string', () => {
+        spyOn(view, '$');
+        subview = view.initSubview(SubView, { el: '.some-selector' });
+        expect(view.$).toHaveBeenCalledWith('.some-selector');
+      });
+    });
+
+    describe('#destroySubviews', () => {
+      let secondSubview;
+
+      beforeEach(() => {
+        secondSubview = view.initSubview(SubView);
+        spyOn(secondSubview, 'remove');
+        spyOn(subview, 'remove');
+      });
+
+      it('should correctly just provided subview', () => {
+        view.destroySubviews(secondSubview);
+        expect(secondSubview.remove).toHaveBeenCalled();
+      });
+
+      it('should correctly destroy all subviews if no exact subview provided', () => {
+        view.destroySubviews();
+        expect(subview.remove).toHaveBeenCalled();
+        expect(secondSubview.remove).toHaveBeenCalled();
+      });
+    });
+
+    describe('#remove', () => {
+      it('should remove subviews correctly', () => {
+        spyOn(subview, 'remove').and.callThrough();
+        view.remove();
+
+        expect(subview.remove).toHaveBeenCalled();
+        expect(subviewCount(view)).toBe(0);
+      });
+
+      it('should remove all subviews on #remove()', () => {
+        view.remove();
+        expect(subviewCount(view)).toBe(0);
+      });
+
+      it('should remove subviews of subviews correctly', () => {
+        let subviewOfSubview = subview.initSubview(SubView);
+        expect(subviewCount(subviewOfSubview)).toBe(0);
+
+        view.remove();
+        expect(subviewCount(subviewOfSubview)).toBe(0);
+      });
+
+      it('should call Backbone.prototype.remove on #remove()', () => {
+        spyOn(Backbone.View.prototype, 'remove');
+        view.remove();
+        expect(Backbone.View.prototype.remove).toHaveBeenCalled();
+      });
+
+      it('should remove reference from parent view if subview was removed directly', () => {
+        subview.remove();
+        expect(subviewCount(view)).toBe(0);
+      });
+    });
   });
 
-  describe('#initSubview', () => {
-    it('should instantiate subview correctly', () => {
-      expect(subview.initialize).toHaveBeenCalled();
-      expect(subviewCount(view)).toBe(1);
-    });
-
-    it('should return instance of subview', () => {
-      expect(subview instanceof SubView).toBe(true);
-    });
-
-    it('should instantiate inside parent view if .el is a string', () => {
-      spyOn(view, '$');
-      subview = view.initSubview(SubView, { el: '.some-selector' });
-      expect(view.$).toHaveBeenCalledWith('.some-selector');
-    });
-  });
-
-  describe('#destroySubviews', () => {
-    let secondSubview;
+  describe('Subview events', () => {
+    let view;
 
     beforeEach(() => {
-      secondSubview = view.initSubview(SubView);
-      spyOn(secondSubview, 'remove');
-      spyOn(subview, 'remove');
+      let View = Backbone.View.extend(ChildMixin);
+
+      let RootView = View.extend({
+        bubbleEvents: {
+          'plus': '_onPlus',
+          'minus': '_onMinus'
+        },
+
+        _onPlus(num) {
+          this.sum += num;
+        },
+
+        _onMinus(num) {
+          this.sum -= num;
+        },
+
+        initialize() {
+          this.sum = 0;
+          this.subview = this.initSubview(SubView);
+          this.subviewWithoutBubble = this.initSubview(View);
+        }
+      });
+
+      let SubView = View.extend({
+        bubbleEvents: {
+          'plus': '_onPlus'
+        },
+
+        _onPlus(num) {
+          this.sum += num;
+        },
+
+        initialize() {
+          this.sum = 0;
+          this.subviewWithoutBubble = this.initSubview(View);
+        }
+      });
+
+      view = new RootView();
     });
 
-    it('should correctly just provided subview', () => {
-      view.destroySubviews(secondSubview);
-      expect(secondSubview.remove).toHaveBeenCalled();
+    it('should handle bubbled events from direct subview', () => {
+      view.subview.trigger('plus', 1);
+      expect(view.sum).toBe(1);
     });
 
-    it('should correctly destroy all subviews if no exact subview provided', () => {
-      view.destroySubviews();
-      expect(subview.remove).toHaveBeenCalled();
-      expect(secondSubview.remove).toHaveBeenCalled();
-    });
-  });
-
-  describe('#remove', () => {
-    it('should remove subviews correctly', () => {
-      spyOn(subview, 'remove').and.callThrough();
-      view.remove();
-
-      expect(subview.remove).toHaveBeenCalled();
-      expect(subviewCount(view)).toBe(0);
+    it('should proxy event without handling it', () => {
+      view.subview.subviewWithoutBubble.trigger('minus', 2);
+      expect(view.sum).toBe(-2);
     });
 
-    it('should remove all subviews on #remove()', () => {
-      view.remove();
-      expect(subviewCount(view)).toBe(0);
-    });
-
-    it('should remove subviews of subviews correctly', () => {
-      let subviewOfSubview = subview.initSubview(SubView);
-      expect(subviewCount(subviewOfSubview)).toBe(0);
-
-      view.remove();
-      expect(subviewCount(subviewOfSubview)).toBe(0);
-    });
-
-    it('should call Backbone.prototype.remove on #remove()', () => {
-      spyOn(Backbone.View.prototype, 'remove');
-      view.remove();
-      expect(Backbone.View.prototype.remove).toHaveBeenCalled();
-    });
-
-    it('should remove reference from parent view if subview was removed directly', () => {
-      subview.remove();
-      expect(subviewCount(view)).toBe(0);
+    it('should proxy events with handling it', () => {
+      view.subview.subviewWithoutBubble.trigger('plus', 1);
+      expect(view.subview.sum).toBe(1);
+      expect(view.sum).toBe(1);
     });
   });
 });
