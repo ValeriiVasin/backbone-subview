3c6a4ecbbe662c5db3c7be4c571edd78aa48fb78
Valerii Vasin <valerii.vasin@gmail.com>
2015-11-06 01:17:54 +0100

    Cleanup webpack config.

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
       // webpack configuration
       module: {
         loaders: [
-          { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
         ]
       }
     },
